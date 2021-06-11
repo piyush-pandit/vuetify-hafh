431f2b4cb42a1630c7a3c5e57719ad04d3333613
piyush <dk0013@digikull.com>
2021-06-11 09:56:14 +0530

    post changes

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,128 +1,134 @@
 <template>
   <div>
-      <v-card>
-        <v-img
-          style="border-radius: 0px 0px 30px 30px;"
-          src="https://sc02.alicdn.com/kf/H14b210d2f37942ea86a6ef2fc81cdc61Y/226479103/H14b210d2f37942ea86a6ef2fc81cdc61Y.png"
-        >
+    <v-card>
+      <v-img
+        style="border-radius: 0px 0px 30px 30px"
+        src="https://sc02.alicdn.com/kf/H14b210d2f37942ea86a6ef2fc81cdc61Y/226479103/H14b210d2f37942ea86a6ef2fc81cdc61Y.png"
+      >
         <!-- <v-card-title><v-icon>mdi-arrow-left</v-icon>Kitchen</v-card-title> -->
-        
-          <v-fab-transition>
-              <v-btn
-                class="btn"
-                style="color:white"
-                top
-                left
-                fixed
-                small
-                light
-                href="/room"
-              >
-                <v-icon
-                  dark
-                  left
-                >
-                  mdi-arrow-left
-                </v-icon> Kitchen
-              </v-btn>
-          </v-fab-transition>
-        </v-img>
-        <v-card-title><strong>Your List</strong></v-card-title>
 
-        <v-list-item>
-          <v-list-item-avatar>
-            <v-img src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png"></v-img>
-          </v-list-item-avatar>
+        <v-fab-transition>
+          <v-btn
+            class="btn"
+            style="color: white"
+            top
+            left
+            fixed
+            small
+            light
+            href="/room"
+          >
+            <v-icon dark left> mdi-arrow-left </v-icon> Kitchen
+          </v-btn>
+        </v-fab-transition>
+      </v-img>
+      <v-card-title><strong>Your List</strong></v-card-title>
 
-          <div style="margin-left: 10px">
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img
+            src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <div style="margin-left: 10px">
           <v-list-item-title class="text-h6">Chintan Shah</v-list-item-title>
           <v-list-item-subtitle>28/05/2021</v-list-item-subtitle>
-          </div>
-          <v-spacer />
-          <v-btn 
-            outlined
-            rounded
-            color="#ffab01"
-            dark
-            small
-          >
-          Add to Old
+        </div>
+        <v-spacer />
+  
+        <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon style="color: #ffab01">mdi-dots-vertical</v-icon>
           </v-btn>
-        </v-list-item>
+        </template>
+        <v-list>
+          <v-btn outlined rounded color="#ffab01" dark small> Add to Old </v-btn><br />
+          <v-btn outlined rounded color="#ffab01" dark small> Delete Old </v-btn>
+          <v-btn outlined rounded color="#ffab01" dark small> post this </v-btn>
+        </v-list> 
+        </v-menu>
+      </v-list-item>
+      
 
-        <div>
+      <div>
         <v-img
-          style="border-radius: 20px; margin:5px"
+          style="border-radius: 20px; margin: 5px"
           src="https://learn.compactappliance.com/wp-content/uploads/2017/12/steamed-broccoli.jpg"
         >
         </v-img>
-        <p style="margin-right:60%">Buy Broccoli</p><br/>
+        <p style="margin-right: 60%">Buy Broccoli</p>
+        <br />
         <p class="line"><span>Old</span></p>
-        </div>
+      </div>
 
-        <v-list-item>
-          <v-list-item-avatar>
-            <v-img src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png"></v-img>
-          </v-list-item-avatar>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img
+            src="https://miro.medium.com/max/600/1*PiHoomzwh9Plr9_GA26JcA.png"
+          ></v-img>
+        </v-list-item-avatar>
 
-          <div style="margin-left: 10px">
+        <div style="margin-left: 10px">
           <v-list-item-title class="text-h6">Chintan Shah</v-list-item-title>
           <v-list-item-subtitle>28/05/2021</v-list-item-subtitle>
-          </div>
-        </v-list-item>
+        </div>
+      </v-list-item>
 
-        <div>
+      <div>
         <v-img
-          style="border-radius: 20px; margin:5px"
+          style="border-radius: 20px; margin: 5px"
           src="http://images2.fanpop.com/images/photos/5400000/Random-Food-random-5409310-1280-800.jpg"
         >
         </v-img>
-        </div>
+      </div>
 
-        <div>
-          <v-card-text >
-            <v-fab-transition >
-              <v-btn
-                color="#ffab01"
-                dark
-                right
-                fab
-                fixed 
-                bottom
-                href="/postmodal"
-              >
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-              <!-- <v-subtitle>New Post</v-subtitle> -->
-            </v-fab-transition>
-          </v-card-text>
-        </div>
-
-      </v-card>
+      <div>
+        <v-card-text>
+          <v-fab-transition>
+            <v-btn
+              color="#ffab01"
+              dark
+              right
+              fab
+              fixed
+              bottom
+              href="/postmodal"
+            >
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+            <!-- <v-subtitle>New Post</v-subtitle> -->
+          </v-fab-transition>
+        </v-card-text>
+      </div>
+    </v-card>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'Post',
-  data () {
-    return {}
-  }
-}
+  name: "Post",
+  // data: () => ({
+  //     items: [
+  //       { title: 'add to old' },
+  //       { title: 'delete old' },
+  //     ],
+  //   }),
+};
 </script>
 
 
 
 <style scoped>
-
 .line {
   width: 100%;
   text-align: center;
   border-bottom: 1px solid #abb7b7;
   line-height: 0.1em;
   margin: 10px 0 20px;
-  color:#abb7b7
+  color: #abb7b7;
 }
 
 .line span {
@@ -130,11 +136,10 @@ export default {
   padding: 0 10px;
 }
 
-.btn{
-  width:120px; 
-  margin: 10px 0px 0px -30px ; 
+.btn {
+  width: 120px;
+  margin: 10px 0px 0px -30px;
   text-transform: capitalize;
-  background: linear-gradient(to right, #1e1f1f 63%, #f6eef1 98%); 
+  background: linear-gradient(to right, #1e1f1f 63%, #f6eef1 98%);
 }
-
 </style>
